ee777d78c88730a119eea4627c649d3f904d40c1
i <i@localhost>
2015-12-07 17:21:08 +0000

    Add some basic checks when activating clipboard

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -9104,6 +9104,20 @@ int iuse::ladder( player *p, item *, bool, const tripoint& )
 
 int iuse::stocktake( player *p, item *it, bool, const tripoint& )
 {
+    static const std::vector<std::string> materials = {{
+                                                               "paper"
+                                                       }};
+    int inventory_index = g->inv_for_filter( _("Write on what?"), [it]( const item &itm ) {
+        return itm.made_of_any( materials ) && itm.has_flag("WRITABLE") && &itm != it;
+    } );
+    item *writable = &( p->i_at( inventory_index ) );
+    if (writable == NULL || writable->is_null()) {
+        p->add_msg_if_player( m_info, _( "You do not have anything you can write on!" ) );
+        return 0;
+    } else if(!writable->has_flag("WRITABLE") || !writable->made_of_any(materials)) {
+        p->add_msg_if_player( m_info, _( "You cannot write on that!" ) );
+        return 0;
+    }
     p->add_msg_if_player(_("Performing stock take."));
     return 0;
 }
