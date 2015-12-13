5e60d05e1b03e1870cc7362f33842cb4e5f1e31f
790 <790@users.noreply.github.com>
2015-12-13 14:01:08 +0000

    create manifest item when stocktake completes

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -9107,11 +9107,28 @@ int iuse::stocktake( player *p, item *it, bool, const tripoint& )
     const int radius = 8;
     std::vector<map_item_stack> items = g->find_nearby_items(radius, false);
 
+    int totalItemCount = 0;
+    std::ostringstream manifestText;
+    manifestText << "Manifest\n--------\n";
     for( map_item_stack &item : items ) {
-        p->add_msg_if_player(_("%s %d"), item.example->tname(item.vIG.size(), false).c_str(), item.totalcount);
+
+        p->add_msg_if_player(_("%s %d"), item.example->tname(item.totalcount, false).c_str(), item.totalcount);
+        totalItemCount += item.totalcount;
+        manifestText << item.example->tname(item.totalcount, false) << " " << item.totalcount << "\n";
     }
 
-    p->add_msg_if_player(_("Performing stock take."));
+    if(totalItemCount == 0) {
+        p->add_msg_if_player(_("Stock take complete. Found no items."));
+        return 0;
+    } else {
+        item manifest("note", 0, false);
+        manifest.set_var("name", "A manifest");
+        manifest.set_var("description", manifestText.str());
+        p->inv.push_back(manifest);
+
+        p->add_msg_if_player(_("Stock take complete. Catalogued %d items."), totalItemCount);
+    }
+
     return it->type->charges_to_use();
 }
 
